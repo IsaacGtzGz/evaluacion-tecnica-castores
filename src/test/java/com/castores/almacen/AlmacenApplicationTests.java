32e71eeb59f618d76c20f91d3a9bc05bcc96a034
IsaacGtzGz <isaacgtzgz@gmail.com>
2026-07-25 17:43:22 -0600

    chore: configuracion inicial del proyecto y conexion a mysql

---
+++ src/test/java/com/castores/almacen/AlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castores.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
